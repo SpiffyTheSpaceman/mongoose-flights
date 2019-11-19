cf34ab84f54ef8fecc6357c716978c281eefb860
James Kim <jameskim@Jamess-MacBook-Pro.local>
2019-11-19 14:12:43 -0800

    Add delete button

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,41 +1,50 @@
-var Flight = require('../models/flight');
+var Flight = require("../models/flight");
 
 module.exports = {
   index,
   new: newFlight,
   show,
   create,
-  delete: deleteFlight,
+  delete: deleteFlight
 };
 
 function index(req, res) {
   Flight.find({}, function(err, flights) {
-    res.render('flights/index', { title: 'All Flights', flights });
+    res.render("flights/index", { title: "All Flights", flights });
   });
-};
+}
 function newFlight(req, res) {
-   var newFlight = new Flight();
-   var dt = newFlight.departs;
-   var destDate = 
-      `${dt.getFullYear()}-${dt.getMonth() + 1}-${dt.getDate()}T${dt.getHours().toString().padStart(2, '0')}:${dt.getMinutes().toString().padStart(2, '0')}`;
-   res.render('flights/new', { title: 'Add Flight', destDate });
-};
+  var newFlight = new Flight();
+  var dt = newFlight.departs;
+  var destDate = `${dt.getFullYear()}-${dt.getMonth() + 1}-${dt.getDate()}T${dt
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${dt
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}`;
+  res.render("flights/new", { title: "Add Flight", destDate });
+}
 function show(req, res) {
-   Flight.findById(req.params.id, function(err, flight) {
-      res.render('flights/show', {title: 'Flight Detail', flight});
-   });
-};
+  Flight.findById(req.params.id, function(err, flight) {
+    res.render("flights/show", { title: "Flight Detail", flight });
+  });
+}
 
 function create(req, res) {
-   var flight = new Flight(req.body);
-   flight.save(function(err) {
-     // one way to handle errors
-     if (err) return res.redirect('/flights/new');
-     // for now, redirect right back to new.ejs
-     res.redirect('/flights');
-   });
- }
+  var flight = new Flight(req.body);
+  flight.save(function(err) {
+    // one way to handle errors
+    if (err) return res.redirect("/flights/new");
+    // for now, redirect right back to new.ejs
+    res.redirect("/flights");
+  });
+}
 
- function deleteFlight(req, res) {
-    
- }
+function deleteFlight(req, res) {
+  //  Flight.deleteOne(req.params.id, (err, result) => console.log(result));
+  Flight.deleteOne({"_id": req.params.id}, function(err, flight) {
+    res.redirect("/flights");
+  });
+  //  res.redirect('/flights');
+}
